cac0e7de4c125a6b44836393e7e8b67814ccf811
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-08-01 21:16:06 +0200

    seeds

---
+++ seeds/products.seed.js
@@ -0,0 +1,26 @@
+require("../config/db.config");
+
+const mongoose = require("mongoose");
+const Product = require("../models/Product.model");
+const products = require("../products.json");
+
+mongoose.connection.once("open", () => {
+  mongoose.connection
+    .dropCollection("products")
+    .then(() => {
+      console.log("DB cleared");
+
+      return Product.create(products);
+    })
+    .then((productDB) => {
+        productDB.forEach((product) => {
+        console.log(`${product.name} has been created`);
+      });
+
+      console.log(`${productDB.length} products have been created`);
+    })
+    .catch((err) => console.error(err))
+    .finally(() => {
+      mongoose.disconnect();
+    });
+});
