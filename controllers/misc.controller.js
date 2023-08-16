4444b4ade44c0e2190d75f25810d18a9a0bd8fe4
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-08-16 18:17:45 +0200

    Home changes

--- controllers/misc.controller.js
+++ controllers/misc.controller.js
@@ -3,7 +3,7 @@ module.exports.goHome = (req, res, next) => {
   //   res.render("misc/home");
 
   Product.find()
-    .limit(10)
+    .limit(3)
     .then((products) => {
       res.render("misc/home", { products });
     })
