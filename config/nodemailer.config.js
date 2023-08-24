2d34c73de486bea52a10773bc9f550fc1e37f874
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-08-24 19:11:40 +0200

    Mail con Ticket info

--- config/nodemailer.config.js
+++ config/nodemailer.config.js
@@ -1,67 +1,99 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 const email = process.env.EMAIL_ACCOUNT;
 const password = process.env.EMAIL_PASSWORD;
 const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+  service: "Gmail",
   auth: {
     user: email,
     pass: password,
   },
 });
+
 module.exports.sendValidationEmail = (user) => {
-  console.log(user)
-  console.log('user ID is: ' + user._id)
+  console.log(user);
+  console.log("user ID is: " + user._id);
   transporter
     .sendMail({
-      from: `"PadelHack" <${email}>`, // sender address
-      to: user.email, // list of receivers
-      subject: 'Welcome to PadelHack', // Subject line
+      from: `"PadelHack" <${email}>`,
+      to: user.email,
+      subject: "Welcome to PadelHack",
       html: `
-                <h1>Welcome to PadelHack</h1>
-                <p>Activate your account</p>
-                <a href="${process.env.APP}/users/${user._id}/activate">Click here</a>
-              `,
+        <h1>Welcome to PadelHack</h1>
+        <p>Activate your account</p>
+        <a href="${process.env.APP}/users/${user._id}/activate">Click here</a>
+      `,
     })
     .then(() => {
       console.log(`email sent to ${user._id}`);
     })
     .catch((err) => {
-      console.error('error sending mail', err);
+      console.error("error sending mail", err);
     });
 };
-const parseProducts = (products) => {
-  console.log(products);
-  return products.map((product) => {
-    return `
-      <li>
-        <h3>${product.product.name}</h3>
-        <p>Precio: ${product.product.price} €</p>
-        <p>Cantidad: ${product.quantity}</p>
-      </li>
-    `;
-  });
-};
+
 module.exports.sendTicketEmail = (ticket) => {
-  console.log(ticket)
-  console.log('ticket ID is: ' + ticket._id)
+  console.log(ticket);
+  console.log("ticket ID is: " + ticket._id);
+
+  const productsTable = parseProducts(ticket.products).join("");
+  const totalSubtotal = ticket.products.reduce(
+    (total, product) => total + product.subtotal,
+    0
+  );
+
   transporter
     .sendMail({
-      from: `"PadelHack" <${email}>`, // sender address
-      to: ticket.buyer.email, // list of receivers
-      subject: 'Your purchase', // Subject line
+      from: `"PadelHack" <${email}>`,
+      to: ticket.buyer.email,
+      subject: "Confirmación de Compra",
       html: `
-                <h1>Confirmación de Compra</h1>
-                <h3>Gracias por confiar en Padelhack.</h3>
-                <h3>Aquí tienes los detalles de tu pedido.</h3>
-                <p>Productos: </p>
-                ${parseProducts(ticket.products)}
-                <p>Total: ${ticket.total}</p>
-              `,
+        <h2>Confirmación de Compra</h2>
+        <p>Gracias por confiar en Padelhack.</p>
+        <p>Aquí tienes los detalles de tu pedido.</p>
+        <h3>Productos:</h3>
+        <table style="border-collapse: collapse; width: 100%;">
+          <thead>
+            <tr>
+              <th style="border: 1px solid black; padding: 8px; text-align: center;">Producto</th>
+              <th style="border: 1px solid black; padding: 8px; text-align: center;">Imagen</th>
+              <th style="border: 1px solid black; padding: 8px; text-align: center;">Precio</th>
+              <th style="border: 1px solid black; padding: 8px; text-align: center;">Cantidad</th>
+              <th style="border: 1px solid black; padding: 8px; text-align: center;">Subtotal</th>
+            </tr>
+          </thead>
+          <tbody>
+            ${productsTable}
+            <tr>
+              <td colspan="4" style="border: 1px solid black; padding: 8px; text-align: right;"><strong>Total:</strong></td>
+              <td style="border: 1px solid black; padding: 8px; text-align: center;"><strong>${totalSubtotal} €</strong></td>
+            </tr>
+          </tbody>
+        </table>
+        <br />
+        <h3 style="text-align: center;">El equipo de Padelhack</h3>
+      `,
     })
     .then(() => {
       console.log(`email sent to ${ticket.buyer.email}`);
     })
     .catch((err) => {
-      console.error('error sending mail', err);
+      console.error("error sending mail", err);
     });
+};
+
+const parseProducts = (products) => {
+  console.log(products);
+  return products.map((product) => {
+    const subtotal = product.product.price * product.quantity;
+    product.subtotal = subtotal;
+    return `
+      <tr>
+        <td style="border: 1px solid black; padding: 8px; text-align: center;">${product.product.name}</td>
+        <td style="border: 1px solid black; padding: 8px; text-align: center;"><img src="${product.product.image}" alt="${product.product.name}" style="max-width: 100px;"></td>
+        <td style="border: 1px solid black; padding: 8px; text-align: center;">${product.product.price} €</td>
+        <td style="border: 1px solid black; padding: 8px; text-align: center;">${product.quantity}</td>
+        <td style="border: 1px solid black; padding: 8px; text-align: center;">${subtotal} €</td>
+      </tr>
+    `;
+  });
 };
--- models/Ticket.model.js
+++ models/Ticket.model.js
@@ -1,34 +1,33 @@
 const mongoose = require("mongoose");
 
-const ticketSchema = new mongoose.Schema({
-  products: {
-    type: [
-      {
-        product: {
-          type: mongoose.Types.ObjectId,
-          ref: "Product",
-          required: false,
+const ticketSchema = new mongoose.Schema(
+  {
+    products: {
+      type: [
+        {
+          product: {
+            type: mongoose.Types.ObjectId,
+            ref: "Product",
+            required: false,
+          },
+          quantity: {
+            type: Number,
+            default: 0,
+          },
         },
-        quantity: {
-          type: Number,
-          default: 0
-        }
-      
-      },
-    ]
+      ],
+    },
+    buyer: {
+      type: mongoose.Types.ObjectId,
+      ref: "User",
+      required: false,
+    },
+    total: {
+      type: Number,
+      default: 0,
+    },
   },
-  buyer: {
-    type: mongoose.Types.ObjectId,
-    ref: "User",
-    required: false,
-  },
-  total: {
-    type: Number,
-    default: 0
-  },
-  
-},
-{ timestamps: true}
+  { timestamps: true }
 );
 
 const Tickets = mongoose.model("Ticket", ticketSchema);
